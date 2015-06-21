ce9a00c09384ca95f903ee6e598655aaedd31033
Andreas Kompanez <akm@mm.st>
2015-06-21 15:12:04 +0200

    Carthage support
    A dynamic framework is required for that

---
+++ MarkdownParser/MarkdownParser.h
@@ -0,0 +1,15 @@
+//
+//  MarkdownParser.h
+//  MarkdownParser
+//
+//  Created by Andreas Kompanez on 21.06.15.
+//  Copyright (c) 2015 Computertalk Sweden. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+
+#import <MarkdownParser/TSMarkdownParser.h>
+
+
