7e0a5ccb52525365a9aca600a3bf58f815353b56
Jure Lajlar <jlajlar@gmail.com>
2020-08-18 10:48:22 +0200

    Added public header file.

---
+++ TSMarkdownParser/include/MarkdownParser.h
@@ -0,0 +1,14 @@
+//
+//  MarkdownParser.h
+//  
+//
+//  Created by Jure Lajlar on 18/08/2020.
+//
+
+#ifndef MarkdownParser_h
+#define MarkdownParser_h
+
+#import "../TSBaseParser.h"
+#import "../TSMarkdownParser.h"
+
+#endif /* MarkdownParser_h */
